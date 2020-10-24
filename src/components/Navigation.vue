07cc27c12cb2099b68ea00537b8f6c4f6aa49528
Teodor Radulovic <radulovicteodor@gmail.com>
2020-10-24 16:03:23 +0200

    initial scss setup and navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="nav">
+        <div class="content wrapper flex-layout">
+            <!-- logo -->
+            <img class="logo" :src="logo" alt="logo">
+
+            <!-- search item -->
+            <div class="search-item">
+                <p>Search items</p>
+                <p>Post items</p>
+            </div>
+
+            <!-- status information -->
+            <div class="status-info">
+                
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+/* Import logo */
+import logo from "@/assets/media/logo-jsguru.png";
+
+export default {
+    name: "Navigation",
+
+    data() {
+        return {
+            logo
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .nav {
+        display: flex;
+        background: $primary-bg-color;
+
+        img.logo {
+            width: 180px;
+            height: auto;
+        }
+    }
+</style>
